84b772f5d0455fb3c604ac40538c547d980daa42
ezu0 <h-ezure@est.co.jp>
2022-04-18 16:25:08 +0900

    drag and drop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
-import './App.css';
+import { Draggable } from 'react-beautiful-dnd';
+import { Droppable } from 'react-beautiful-dnd';
+import { DragDropContext } from 'react-beautiful-dnd';
+import './App.css'
 
-function App() {
+const  App =() => {
   return (
-    <div>
-      Hello world
+    <div className="dragDropArea">
+      <DragDropContext>
+        <Droppable droppableId='droppable'>
+          {(provided) =>(
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              <Draggable draggableId='item0' index={0}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    item0
+                  </div>
+                )}
+              </Draggable>
+              <Draggable draggableId='item1' index={1}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    item1
+                  </div>
+                )}
+              </Draggable>
+              <Draggable draggableId='item2' index={2}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    item2
+                  </div>
+                )}
+              </Draggable>
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }

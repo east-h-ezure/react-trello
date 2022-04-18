ac5fab65147f33d63a54f514c019c76484455784
ezu0 <h-ezure@est.co.jp>
2022-04-18 16:36:29 +0900

    add header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import Header from './components/Header';
 
-function App() {
+const App = () => {
   return (
     <div>
-      Hello world
+      <Header />
     </div>
   );
 }

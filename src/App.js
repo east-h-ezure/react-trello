86328c8397a1da1379caa8d2457ea28711fc6fd2
ezu0 <h-ezure@est.co.jp>
2022-04-18 16:53:18 +0900

    add: TaskCards component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import TaskCards from './components/task/TaskCards';
 
 const App = () => {
   return (
     <div className="app">
       <Header />
+      <TaskCards />
     </div>
   );
 }

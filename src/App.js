04e454731f3d4d03f8e0961c01e1f4cacd029529
ezu0 <h-ezure@est.co.jp>
2022-04-18 15:19:45 +0900

    add: map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
-
 function App() {
+
+  const list = [1, 3, 5, 6];
+  const dataList = [
+    {
+      name: 'taro',
+      age: 21
+    },
+    {
+      name: 'hanako',
+      age: 23
+    }
+  ]
   return (
     <div>
-      Hello world
+      <h1>Map Plactice</h1>
+      {list.map((number) => (
+        <div>
+          {number * 2}
+        </div>
+      ))}
+      {dataList.map((data) => (
+        <div>
+          <div>{data.name}</div>
+          <div>{data.age}</div>
+        </div>
+      ))}
     </div>
   );
 }
